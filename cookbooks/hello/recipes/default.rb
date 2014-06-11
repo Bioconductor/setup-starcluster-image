95f7301fedbca73f75c7602abe3dd633da518c83
Dan Tenenbaum <dtenenba@fhcrc.org>
2014-06-10 22:24:09 -0700

    some progress towards installing R

--- cookbooks/hello/recipes/default.rb
+++ cookbooks/hello/recipes/default.rb
@@ -20,9 +20,112 @@
 # If a message was provided then display it; otherwise, say 'Hello World'.
 message = node.has_key?(:message) ? node[:message] : "Hello World"
 
+# config.ssh.username
+
+require 'yaml'
+
+configfile = "/vagrant/config.yml"
+unless File.exists? configfile
+    raise "config.yml does not exist!"
+end
+
+yamlconfig = YAML.load_file configfile
+
+unless yamlconfig.has_key? "r_version"
+    raise "no 'r_version' key in config.yml!"
+end
+
+unless yamlconfig.has_key? "use_devel"
+    raise "no 'use_devel' key in config.yml!"
+end
+
+unless [FalseClass, TrueClass].include? yamlconfig['use_devel'].class
+    raise "'use_devel' key  in config.yml has invalid value (must be TRUE or FALSE)!"
+end
+
+
+# CRAN_RSOURCE_TOPDIR="R-devel"
+# CRAN_TARBALL="$CRAN_RSOURCE_TOPDIR.tar.gz"
+# CRAN_TARBALL_URL="ftp://ftp.stat.math.ethz.ch/Software/R/$CRAN_TARBALL"
+#CRAN_RSOURCE_TOPDIR="R-3.1.0"
+#CRAN_TARBALL="R-3.1.0.tar.gz"
+#CRAN_TARBALL_URL="http://cran.r-project.org/src/base/R-3/$CRAN_TARBALL"
+
+r_version = yamlconfig["r_version"]
+tarball = "#{r_version}.tar.gz"
+
+tarball_url = nil
+if r_version =~ /^R-[0-9]/
+    tarball_url = "http://cran.r-project.org/src/base/R-#{r_version.split("")[2]}/#{tarball}"
+else
+    tarball_url = "ftp://ftp.stat.math.ethz.ch/Software/R/#{tarball}"
+end
+
+
+remote_file "/tmp/#{tarball}" do
+    source tarball_url
+end
+
 execute "echo message" do
   command "echo '#{message}'"
   #command "touch /home/vagrant/futz"
   action :run
 end
 
+execute "apt-get update" do
+  command "apt-get update"
+  user "root"
+  action :run
+  # how to guard?
+end
+
+
+execute "install R build deps" do
+  command "apt-get build-dep -y r-base"
+  user "root"
+  action :run
+  not_if 'dpkg --get-selections|grep -q "^xvfb\s"'
+end
+
+execute "untar R tarball" do
+    command "tar zxf #{tarball}"
+    cwd "/tmp"
+    user "root"
+    rdir = "/tmp/#{r_version}"
+    not_if File.exists? rdir and File.directory? rdir
+end
+
+execute "configure R" do
+    command "./configure --enable-R-shlib"
+    cwd "/tmp/#{r_version}"
+    user "root"
+    only_if(!File.exists? "/tmp/#{r_version}/config.log")
+    #not_if "tail -1 /tmp/#{r_version}/config.log|grep -q 'configure: exit 0'"
+end
+
+execute "make R" do
+    command "make -j > /tmp/R-make.out 2>&1"
+    cwd "/tmp/#{r_version}"
+    user "root"
+    not_if File.exists? "/tmp/#{r_version}/bin/R"
+end
+
+execute "install R" do
+    command "make install"
+    cwd "/tmp/#{r_version}"
+    user "root"
+    not_if File.exists? "/usr/local/bin/R"
+end
+
+execute "install R packages" do
+    command "Rscript /vagrant/install.R"
+    environment({"USE_DEVEL" => yamlconfig['use_devel'].to_s.upcase})
+    user "root"
+    not_if 'ls /usr/local/lib/R/library| grep -q "^VariantAnnotation$"'
+end
+
+# libdbi-perl libdbd-mysql-perl
+
+# install rstudio server
+
+# clear history....
---
+++ install.R
@@ -0,0 +1,12 @@
+#!/usr/bin/env Rscript
+
+# install BioC packages
+
+source("http://bioconductor.org/biocLite.R")
+
+if (Sys.getenv("USE_DEVEL") == "TRUE")
+    useDevel()
+
+#affxparser affy affyio affylmGUI annaffy annotate AnnotationDbi aroma.light BayesPeak baySeq Biobase BiocInstaller biomaRt Biostrings BSgenome Category ChIPpeakAnno chipseq ChIPseqR ChIPsim CSAR cummeRbund DESeq DEXSeq DiffBind DNAcopy DynDoc EDASeq edgeR ensemblVEP gage genefilter geneplotter GenomeGraphs genomeIntervals GenomicFeatures GenomicRanges Genominator GEOquery GGBase GGtools girafe goseq GOstats graph GSEABase HilbertVis impute IRanges limma MEDIPS multtest oneChannelGUI PAnnBuilder preprocessCore qpgraph qrqc R453Plus1Toolbox RBGL Repitools rGADEM Rgraphviz Ringo Rolexa Rsamtools Rsubread rtracklayer segmentSeq seqbias seqLogo ShortRead snpStats splots SRAdb tkWidgets VariantAnnotation vsn widgetTools zlibbioc
+
+#Rmpi
